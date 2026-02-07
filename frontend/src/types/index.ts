6203f4acf0bb0e43adae422511721a799f23f2fc
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-02-07 16:27:47 +0000

    add interfaces in index.ts for backend data types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,5 +1,12 @@
-export interface Metrics {
-  total_stock: number;
+export interface Products {
+  name: string;
+  stock_quantity: number;
   total_sold: number;
-  total_gains_after_taxes: number;
+  price: number; 
+}
+
+export interface Metrics {
+  total_stock: number;               
+  total_sold: number;                
+  total_gains_after_taxes: number;   
 }
