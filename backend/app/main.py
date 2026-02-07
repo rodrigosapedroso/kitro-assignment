c59f1e393130e99ff7296a0f013e6e16b492f75e
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-02-07 20:53:25 +0000

    added CORS middleware in main.py file to allow requests from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from . import routers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Kitro")
 
@@ -16,3 +17,13 @@ app.include_router(routers.router)
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
+
+#Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
