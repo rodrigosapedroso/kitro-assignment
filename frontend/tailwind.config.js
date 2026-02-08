d22f9b3dfba403c00ad454e2317b662298204ef6
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-02-08 23:22:10 +0000

    import kitro colors and defining inter as an overall font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,40 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kitro: {
+          primary: "#ED615F",
+          primaryDark: "#E93A37",
+          accent: "#FFDDA7",
+
+          background: "#FBF9F4",
+          backgroundAlt: "#F7F3E8",
+          surface: "#F3F3EF",
+          surfaceAlt: "#E1E1D6",
+
+          border: "#D3D1C9",
+          borderLight: "#CECEC5",
+
+          textPrimary: "#323232",
+          textSecondary: "#6E6E68",
+          textMuted: "#9E9E96",
+
+          grayLight: "#F7F7F7",
+          gray: "#DBDBDB",
+          grayDark: "#7F7F7F",
+
+          success: "#507E6A",
+          successLight: "#C6e6e3",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
